30c6c9006379d9e1b021ca4e65a3ee37d46b56c2
oykunyenal <oykun.yenal@gmail.com>
2024-01-22 23:13:36 +0300

    react admin dashboard user grid added to the project.

---
+++ src/components/dataTable/DataTable.tsx
@@ -0,0 +1,41 @@
+import "./datatable.scss";
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
+
+type Props = {
+  columns: GridColDef[];
+  rows: object[];
+};
+
+const DataTable = (props: Props) => {
+  return (
+    <div className="dataTable">
+      <DataGrid
+        className="dataGrid"
+        rows={props.rows}
+        columns={props.columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        slots={{ toolbar: GridToolbar }}
+        slotProps={{
+          toolbar: {
+            showQuickFilter: true,
+            quickFilterProps: { debounceMs: 500 },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+        disableColumnFilter
+        disableDensitySelector
+        disableColumnSelector
+      />
+    </div>
+  );
+};
+
+export default DataTable;
--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,7 +1,77 @@
+import { GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import DataTable from "../../components/dataTable/DataTable";
 import "./users.scss";
+import { userRows } from "../../data";
 
 const Users = () => {
-  return <div className="users">Users</div>;
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "avatar",
+      headerName: "Avatar",
+      width: 100,
+      renderCell: (params) => {
+        return <img src={params.row.img || "/noavatar.png"}></img>;
+      },
+    },
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 100,
+      renderCell: (params) => {
+        return (
+          <div className="action">
+            <div className="view">View</div>
+            <div className="delete">Delete</div>
+          </div>
+        );
+      },
+    },
+    {
+      field: "verified",
+      headerName: "Status",
+      width: 100,
+      type: "boolean",
+    },
+    {
+      field: "firstName",
+      headerName: "First name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "lastName",
+      headerName: "Last name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      width: 110,
+      editable: true,
+    },
+    {
+      field: "fullName",
+      headerName: "Full name",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      valueGetter: (params: GridValueGetterParams) =>
+        `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    },
+  ];
+
+  return (
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New User</button>
+      </div>
+      <DataTable columns={columns} rows={userRows} />
+    </div>
+  );
 };
 
 export default Users;
